e860304d2e0064802cde81b36528fdbcccc02fa6
gallodustin <48537601+gallodustin@users.noreply.github.com>
2019-04-27 13:29:37 -0500

    Add files via upload
    add multithreading

--- dustins.c
+++ dustins.c
@@ -1,45 +1,68 @@
-//
-// Created by Dustin Gallo on 2019-04-21.
-//
-
 #include "embedded.h"
 #include <stdio.h>
+#include <pthread.h>
 
 #define error(msg) {fprintf(stderr, "Failure: %s\n", msg); return -1;}
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+    int numthreads = argv[1];
+
     char* err = 0;
     void* conn = 0;
     monetdb_result* result = 0;
     size_t r, c;
 
     // first argument is a string for the db directory or NULL for in-memory mode
+    // we should try performance tests with both!
     err = monetdb_startup(NULL, 0, 0);
-    if (err != 0)
-    error(err)
+    if (err != 0) {
+        error(err)
+    }
 
     conn = monetdb_connect();
-    if (conn == NULL)
-    error("Connection failed")
+    if (conn == NULL) {
+        error("Connection failed")
+    }
 
-    err = monetdb_query(conn, "CREATE TABLE test (app string, category string, rating string, reviews string, "
-                              "size string, installs string, type string, price string, contentR string, "
+    err = monetdb_query(conn, "CREATE TABLE test (app string, category string, rating string, reviews string,"
+                              "size string, installs string, type string, price string, contentR string,"
                               "genres string, lastU string, curV string, andV string)", 1, NULL, NULL, NULL);
-    if (err != 0)
-    error(err)
+    if (err != 0) {
+        error(err)
+    }
 
-    err = monetdb_query(conn,
-                        "COPY 100 RECORDS INTO test FROM '/users/dgal/MonetDBLite-C/googlePlay/googleplaystore.csv' USING"
-                        "DELIMITERS ',','\\n','\"' ", 1, NULL, NULL, NULL);
-    if (err != 0)
-    error(err)
+    err = monetdb_query(conn, "COPY 100 RECORDS INTO test FROM '/users/dgal/googlePlay/googleplaystore.csv'"
+                              "USING DELIMITERS ',','\\n','\"' NULL AS '';", 1, NULL, NULL, NULL);
+    if (err != 0) {
+        error(err)
+    }
 
-    err = monetdb_query(conn, "SELECT * FROM test WHERE app = 'Coloring book moana'; ", 1, &result, NULL, NULL);
-    if (err != 0)
-    error(err)
+    //
+    // dustin's multi-threading code
+    //
 
-    fprintf(stdout, "Query result with %d cols and %d rows\n", (int) result->ncols,
-            (int) result->nrows);
+    pthread_t id[numthreads];
+    int i,rc;
+
+    for (i = 0; i < numthreads; i++) {
+        rc = pthread_create(&id[i], NULL, &editRow, conn);
+        if (rc != 0) {
+            printf("Thread creation failed.");
+            return 0;
+        }
+    }
+
+    for (i = 0; i < numthreads; i++) {
+        pthread_join(id[i], NULL);
+    }
+
+    err = monetdb_query(conn, "SELECT * FROM test WHERE app = 'Coloring book moana';", 1, &result, NULL, NULL);
+    if (err != 0) {
+        error(err)
+    }
+
+    fprintf(stdout, "Query result with %d cols and %d rows\n", (int) result->ncols, (int) result->nrows);
 
     for (r = 0; r < result->nrows; r++) {
         for (c = 0; c < result->ncols; c++) {
@@ -67,7 +90,6 @@ int main(void) {
                     printf("UNKNOWN");
                 }
             }
-
             if (c + 1 < result->ncols) {
                 printf(", ");
             }
@@ -78,5 +100,19 @@ int main(void) {
     monetdb_cleanup_result(conn, result);
     monetdb_disconnect(conn);
     monetdb_shutdown();
+
+    return 0;
+}
+
+int editRow(char* conn) {
+
+    char* err;
+
+    err = monetdb_query(conn, "UPDATE test SET category = 'newval' WHERE app = 'Coloring book moana';",
+            1, NULL, NULL, NULL);
+    if (err != 0) {
+        error(err)
+    }
+
     return 0;
 }
